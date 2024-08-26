e748b28b6f898325c755bc39063e37ab4e701bd9
Jerson Herrera <jeysonhwr@gmail.com>
2024-08-25 20:02:56 -0500

    Back terminado, solo falta agregar mas schemas para validar los datos

--- Back/src/routes/auth.routes.js
+++ Back/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { login, register, logout, profile } from "../controllers/authController.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validatorMiddleware.js";
+import { loginSchema, registerSchema } from "../schemas/authSchema.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
 router.get("/profile",authRequired, profile);
 
---
+++ Back/src/schemas/authSchema.js
@@ -0,0 +1,37 @@
+//Zod nos permite crear esquemas de validacion como en mongoose, pero esste caso validamos los datos recibidos.
+import { z } from "zod";
+
+export const registerSchema = z.object({
+    username: z
+        .string({
+            required_error: "Username is required",
+        })
+        .min(4, "El username debe tener al menos 4 letras"),
+    email: z
+        .string({
+            required_error: "Email is required",
+        })
+        .email({
+            message: "Invalid email",
+        }),
+    password: z
+        .string({
+            required_error: "Password is required",
+        })
+        .min(4, "El password debe tener un minimo de 6 caracteres"),
+});
+
+export const loginSchema = z.object({
+    email: z
+        .string({
+            required_error: "Email is required",
+        })
+        .email({
+            message: "Invalid email",
+        }),
+    password: z
+        .string({
+            required_error: "Password is required",
+        })
+        .min(4, "El password debe tener un minimo de 6 caracteres"),
+});
